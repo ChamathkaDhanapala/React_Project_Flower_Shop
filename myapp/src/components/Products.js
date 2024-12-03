85d9242732673bae1c0d6e6c520003c4aa706da6
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2024-12-03 21:26:12 +0530

    Update Products.js file

--- myapp/src/components/Products.js
+++ myapp/src/components/Products.js
@@ -1,10 +1,25 @@
-
 import '../assets/CSS/layout.css';
+import { useState } from 'react';
+import Product from './Product'; 
+import Cart from './Cart';
+import { flowers } from './FlowerDB'; 
 
+export default function Products() {
+    const [cart, setCart] = useState([]);
 
-export default function Products(){
-   
-    return(
+    
+    const addToCart = (product, quantity) => {
+        const existingProduct = cart.find(item => item.id === product.id);
+        if (existingProduct) {
+            setCart(cart.map(item =>
+                item.id === product.id ? { ...item, qty: item.qty + quantity } : item
+            ));
+        } else {
+            setCart([...cart, { ...product, qty: quantity }]);
+        }
+    };
+
+    return (
         <>
             <div className="item1">
                 <h1>Flower Shop</h1>
@@ -12,18 +27,14 @@ export default function Products(){
             <div className="item2">
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
-                    {
-                        //product
-                    }
+                    {flowers.map(flower => (
+                        <Product key={flower.id} flower={flower} addToCart={addToCart} />
+                    ))}
                 </div>
-
             </div>
             <div className="item3">
-                {
-                //cart
-                }
+                <Cart cart={cart} />
             </div>
         </>
     );
-
 }
